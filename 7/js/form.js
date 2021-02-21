caf8ee9c6f6180b51b07207e0ad2e0f2e830ccfe
Keks <keks@htmlacademy.ru>
2021-02-21 21:33:59 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/form.js
@@ -0,0 +1,50 @@
+const MIN_HOUSE_PRICE = {
+  bungalow: 0,
+  flat: 1000,
+  house: 5000,
+  palace: 10000,
+};
+
+const adForm = document.querySelector('.ad-form');
+const typeFlat = adForm.querySelector('#type');
+const priceFlat = adForm.querySelector('#price');
+const timeIn = adForm.querySelector('#timein');
+const timeOut = adForm.querySelector('#timeout');
+const fieldsets = adForm.querySelectorAll('fieldset');
+const address = adForm.querySelector('#address');
+
+const disableForm = () => {
+  adForm.classList.add('ad-form--disabled');
+  for (let fieldset of fieldsets) {
+    fieldset.disabled = true;
+  }
+};
+
+disableForm();
+
+const activeForm =() => {
+  adForm.classList.remove('ad-form--disabled');
+  for (let fieldset of fieldsets) {
+    fieldset.disabled = false;
+  }
+};
+
+const validatePrice = () => {
+  priceFlat.placeholder = MIN_HOUSE_PRICE[typeFlat.value];
+  priceFlat.min = MIN_HOUSE_PRICE[typeFlat.value];
+  priceFlat.required = true;
+};
+
+typeFlat.addEventListener('change', validatePrice);
+
+const validateCheckIn = () => {
+  timeIn.addEventListener('change', () => timeOut.value = timeIn.value);
+  timeOut.addEventListener('change', () => timeIn.value = timeOut.value);
+};
+
+
+
+validatePrice();
+validateCheckIn();
+
+export {activeForm, address};
---
+++ 7/js/main.js
@@ -0,0 +1,6 @@
+import './util.js';
+import './data.js';
+import './card.js';
+import './form.js';
+import './filters.js';
+import './map.js';
